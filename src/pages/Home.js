e535c9edd3ae647e2eabd634247fbebd2faf2f74
nguyenhung15913 <nguyenhung15913@gmail.com>
2023-10-04 01:31:31 -0600

    added slogan

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,12 @@ function Home () {
           <p>3001 26 Ave SW, Calgary, AB</p>
           <p> T3E 0M7</p>
       </div>
+
+      <div className='Slogan'>
+          <h1>REVEAL YOUR BEAUTY</h1>
+          <h2>BOOK AN APPOINTMENT AND WE’LL TAKE CARE OF THE REST</h2>
+          <p>Duis autem vel eum iriure dolor in hendrerit in putate velit esse molestie consequat, vel illum doloreeu feugiat</p>
+      </div>
     </div>
   )
 }
