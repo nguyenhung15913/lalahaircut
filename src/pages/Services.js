0d4ec2ac9562c1c98acc0479d42e8af497dbbd64
Allan To <le.to@edu.sait.ca>
2023-10-05 23:25:18 -0600

    serviceshours done

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,65 +4,7 @@ export default function Services() {
   return (
     <div className='service'>
         
-    {/*first div */}
-   <div className='firstservice'>
-    <div>
-    <img
-            className="frame-child"
-            alt=""
-            src={require(`./../images/haircut.jpg`)}
-          />
-    </div>
-    <div>
-      Classic haircut
-    </div>
-   </div>
-      {/*end of first div */}
-
- {/*second div */}
- <div className='secondservice'>
-    <div>
-    <img
-            className="frame-child"
-            alt=""
-            src={require(`./../images/dyehair.jpg`)}
-          />
-    </div>
-    <div>
-      Colouring
-    </div>
-   </div>
-      {/*end of second div */}
-
-       {/*third div */}
-   <div className='thirdservice'>
-    <div>
-    <img
-            className="frame-child"
-            alt=""
-            src={require(`./../images/perm.jpg`)}
-          />
-    </div>
-    <div>
-      Perm Hair
-    </div>
-   </div>
-      {/*end of third div */}
-
-       {/*fourth div */}
-   <div className='fourthservice'>
-    <div>
-    <img
-            className="frame-child"
-            alt=""
-            src={require(`./../images/texture.jpg`)}
-          />
-    </div>
-    <div>
-      Texturizing
-    </div>
-   </div>
-      {/*end of fourth div */}
+  <img  className='priceimg' src={require(`./../images/pricelist.png`)}></img>
 
     </div>
   )
